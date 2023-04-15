ea21531e2077794446813a2f56d9260443e02c67
Polina Fomicheva <116427492+polinaburger@users.noreply.github.com>
2023-04-15 14:19:21 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -21,36 +21,34 @@ class TPQueue {
       if (res > size)
         res -= size + 1;
       return res;
-  }
-  
+      }
  public:
- TPQueue():
- begin(0), end(0), count(0) {
-  arr = new T[size + 1];
- }
- ~TPQueue(){
+  TPQueue():
+   begin(0), end(0), count(0) {
+    arr = new T[size + 1];
+    }
+  ~TPQueue() {
   delete[] arr;
- }
+  }
 void push(const T& value) {
   if (isFull()) {
-   throw std::string("is Full!");
+  throw std::string("is Full!");
   } else {
-   int flag = end; 
-   for (int i = begin; i < end; i++) {
-    if (value.prior > arr[i].prior) {
-     flag = i;
-     break;
-    }
-   }
-   for (int i = end; i > flag; i--) {
-    arr[i % size] = arr[(i - 1) % size];
-   }
-   arr[flag % size] = value;
-   count++;
-   end++;
+  int flag = end;
+  for (int i = begin; i < end; i++) {
+  if (value.prior > arr[i].prior) {
+  flag = i;
+  break;
+  }
+  }
+  for (int i = end; i > flag; i--) {
+  arr[i % size] = arr[(i - 1) % size];
+  }
+  arr[flag % size] = value;
+  count++;
+  end++;
   }
  }
- 
 T pop() {
  assert(count > 0);
  T item = arr[begin];
@@ -62,15 +60,15 @@ T get() const {
  assert(count > 0);
  return arr[begin];
 }
- bool isEmpty() const {
-  return count == 0;
+bool isEmpty() const {
+return count == 0;
  }
- bool isFull() const {
-  return count == size;
+bool isFull() const {
+return count == size;
  }
 };
 struct SYM {
-    char ch;
-    int prior;
+ char ch;
+ int prior;
 };
 #endif  // INCLUDE_TPQUEUE_H_
